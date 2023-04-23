b8f6172b785cc6fa63dd86c7c63d03dd29f460e3
willgbryan <will.bryan421@gmail.com>
2023-04-22 19:39:01 -0500

    add multi ticker support

--- data.py
+++ data.py
@@ -8,12 +8,25 @@ data fetches, it is not intended to be a production data source and will
 change frequently.
 """
 
+ticker_list_tech = ["AMZN", "AAPL", "GOOGL"]
+data_df = pd.DataFrame()
+​
 # Example fetch for Amazon data
-amzn_df = yf.Ticker("AMZN")
-amzn_hist = amzn_df.history(period="1mo")
-        
-# Need to double check this derivative attempt but the idea is there
-amzn_hist["derivative"] = amzn_hist["Close"] - amzn_hist["Close"].shift(1)
-plt.plot(amzn_hist.index, amzn_hist["Close"])
-plt.show()
-
+for i in ticker_list_tech:
+​
+    yf_df = yf.Ticker(i)
+    data_hist = yf_df.history(period="10y")
+    data_hist.index.name = "Date"
+    data_hist = data_hist.reset_index().rename(columns={'Date': 'date'})
+    data_hist = data_hist.reset_index(drop=True)
+    data_hist["date"] = pd.DatetimeIndex(data_hist["date"]).strftime('%Y-%m-%d')
+    data_hist = data_hist.reset_index(drop=True)
+    data_hist["date"] = pd.to_datetime(data_hist["date"])
+    data_hist["ticker"] = i
+    data_hist["industry"] = "tech"
+    plt.plot(data_hist.index, data_hist["Close"])
+    plt.show()
+    print(f"dataframe shape ={data_hist.shape}")
+    print(data_hist["date"])
+data_df = data_df.append(data_hist)
+print(data_df)
