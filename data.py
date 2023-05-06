ded7aab105b1fc7e7a26a13d4bbc0a842f3bfef4
willgbryan <107731540+willgbryan@users.noreply.github.com>
2023-05-05 20:52:00 -0500

    support dynamic group and sort column assignment by industry

--- data.py
+++ data.py
@@ -59,34 +59,29 @@ ticker_list_financial_services = [
     "MS",
     "HSBC",
 ]
-ticker_lists = [
-    ticker_list_tech, 
-    ticker_list_market_index, 
-    ticker_list_financial_services,
-    ticker_list_energy,
-]
+ticker_lists = {
+    "tech":ticker_list_tech, 
+    "index":ticker_list_market_index, 
+    "financial":ticker_list_financial_services,
+    "energy":ticker_list_energy,
+}
 
 data_frames = []
 
-#for x in ticker_lists:
-for i in ticker_list_tech:
-    yf_df = yf.Ticker(i)
-    data_hist = yf_df.history(period="10y")
-    data_hist.index.name = "Date"
-    data_hist = data_hist.reset_index().rename(columns={'Date': 'date'})
-    data_hist = data_hist.reset_index(drop=True)
-    data_hist["date"] = pd.DatetimeIndex(data_hist["date"]).strftime('%Y-%m-%d')
-    data_hist = data_hist.reset_index(drop=True)
-    data_hist["date"] = pd.to_datetime(data_hist["date"])
-    data_hist["ticker"] = i
-    data_hist["industry"] = "tech"
-    plt.plot(data_hist["date"], data_hist["Close"])
-    plt.show()
-    print(i)
-    print(f"dataframe shape = {data_hist.shape}")
-    print(data_hist["date"])
-
-    data_frames.append(data_hist)
+for key, value in ticker_lists.items():
+    for i in value:
+        yf_df = yf.Ticker(i)
+        data_hist = yf_df.history(period="10y")
+        data_hist.index.name = "Date"
+        data_hist = data_hist.reset_index().rename(columns={'Date': 'date'})
+        data_hist = data_hist.reset_index(drop=True)
+        data_hist["date"] = pd.DatetimeIndex(data_hist["date"]).strftime('%Y-%m-%d')
+        data_hist = data_hist.reset_index(drop=True)
+        data_hist["date"] = pd.to_datetime(data_hist["date"])
+        data_hist["ticker"] = i
+        data_hist["industry"] = key
+        plt.plot(data_hist["date"], data_hist["Close"])
+        data_frames.append(data_hist)
    
 
 data_df = pd.concat(data_frames, ignore_index=True)
